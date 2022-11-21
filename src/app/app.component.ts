9738e69be1f1daffd399570806aee1e1e25a7201
AmirSeyfollahi <amirseifollahiii@gmail.com>
2022-11-21 11:11:49 +0330

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'audiophile-app';
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'MEASUREMENT-ID', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
