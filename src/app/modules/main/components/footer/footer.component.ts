0cf9059ec0f5ff719b2f0bfa9d5986a8b8770d6d
Amir Seyfollahi <amirseifollahiii@gmail.com>
2025-08-18 16:21:57 +0330

    refactor(footer): align currentPath handling with header and fix home link routing

--- src/app/modules/main/components/footer/footer.component.ts
+++ src/app/modules/main/components/footer/footer.component.ts
@@ -29,10 +29,9 @@ export class FooterComponent implements OnInit {
   getCurrentPath() {
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
-        this.currentPath = val.url;
-
-        if (this.currentPath === '/') {
-          this.currentPath = val.urlAfterRedirects;
+        this.currentPath = val.url === '/' ? val.urlAfterRedirects : val.url;
+        if (val.url === '/' || val.urlAfterRedirects === '/') {
+          this.currentPath = 'home';
         }
       }
     });
